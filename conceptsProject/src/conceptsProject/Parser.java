d92f5809e9f5fa077509352cb7441cc6d9fcd59f
DrewTurn <45215309+DrewTurn@users.noreply.github.com>
2020-05-06 12:03:25 -0500

    Add files via upload

---
+++ conceptsProject/src/conceptsProject/Parser.java
@@ -0,0 +1,61 @@
+package conceptsProject;
+
+import java.util.*;
+
+
+public class Parser {
+
+	public static void Parser(String input) {
+		System.out.println("<Program>");
+		//code to call next is here
+		
+		System.out.println("</Program>");
+	}
+	
+	public static void StatmentList() {
+		System.out.println("<stmt list");
+		//code
+		System.out.println("</stmt list");
+	}
+	
+	public static void Statment() {
+		System.out.println("<stmt");
+		//code
+		System.out.println("</stmt");
+	}
+	public static void expr() {
+		System.out.println("<expr>");
+		//code
+		System.out.println("</expr>");
+	}
+	public static void term_tail() {
+		System.out.println("<term tail>");
+		//code
+		System.out.println("</term tail>");
+	}
+	public static void term() {
+		System.out.println("<term>");
+		//code
+		System.out.println("</term>");
+	}
+	public static void fact_tail() {
+		System.out.println("<fact tail>");
+		//code
+		System.out.println("</fact tail>");
+	}
+	public static void factor() {
+		System.out.println("<factor>");
+		//code
+		System.out.println("</factor>");
+	}
+	public static void add_op() {
+		System.out.println("<add_op>");
+		//code
+		System.out.println("</add_op>");
+	}    
+	public static void mult_op() {
+		System.out.println("<mult op>");
+		//code
+		System.out.println("</mult op>");
+	} 
+}
